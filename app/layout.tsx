3b757ab677cdc6cfd0f8286282ab396dec51c7b2
Bborie Park <dustymugs@gmail.com>
2026-08-09 14:31:06 -0700

    easy enough

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import JsonLd from "@/components/JsonLd";
 import CookieConsent from "@/components/CookieConsent";
 import AnalyticsProvider from "@/components/AnalyticsProvider";
 import ExitIntentPopup from "@/components/ExitIntentPopup";
+import SnitcherTracker from "@/components/SnitcherTracker";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -154,6 +155,7 @@ export default function RootLayout({
           <AnalyticsProvider />
           <ExitIntentPopup />
         </ThemeProvider>
+        <SnitcherTracker />
       </body>
     </html>
   );
